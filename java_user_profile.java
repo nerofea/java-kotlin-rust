9f57565c72a26688a0db8c83e842f60b4b9ee694
SweepTheory <joeyripa@gmail.com>
2025-07-30 17:46:32 -0400

    Update java_user_profile.java

--- java_user_profile.java
+++ java_user_profile.java
@@ -7,14 +7,16 @@ class UserProfile {
     int level;
       /* this is checking to see if the User has premium or not and adding it to the user profile. Im assuming for some type of subscription based application*/
     boolean isPremium;
-
+/*A public class member or constructor is accessible throughout the package where it is declared and from any other package, provided the package in which it is declared is observable*/
     public UserProfile(String username, int level, boolean isPremium) {
+      /*this is assigning the value of username to the variable username, level and isPremium*/
         this.username = username;
         this.level = level;
         this.isPremium = isPremium;
     }
-
+/*public means this can be accessed from anywhere, void indicates that the method does not return a value. He it is outputting the String of username i.e SweepTheory, level-1 and isPremium*/
     public void printProfile() {
+      /*this is outputting the data that was collectued during the userprofile creation to the screen*/
         System.out.println("User: " + username + ", Level: " + level + ", Premium: " + isPremium);
     }
 
