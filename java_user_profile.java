8533c9bb13d2c21910966e68164fa688fa7a5d00
SweepTheory <joeyripa@gmail.com>
2025-07-30 16:28:07 -0400

    Update java_user_profile.java

--- java_user_profile.java
+++ java_user_profile.java
@@ -1,7 +1,12 @@
 class UserProfile {
+    /* the class is defining new reference type. This will take all the data that is stored inside of it and create a UserProfile with whatver the Username, Level and if they pay the premiumfee.
+    This is making a reference for a Userprofile*/
     String username;
+    /* this is taking a string value of whatever the username is and adding it to the user profile*/
     int level;
+    /* this is taking a numberical value of whatever the level is and adding it to the user profile*/
     boolean isPremium;
+    /* this is checking to see if the User has premium or not and adding it to the user profile. Im assuming for some type of subscription based application*/
 
     public UserProfile(String username, int level, boolean isPremium) {
         this.username = username;
