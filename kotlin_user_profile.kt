bf459ece7f15bd3fcc144b85cb64544cb89f59f0
SweepTheory <joeyripa@gmail.com>
2025-08-01 14:03:22 -0400

    Update kotlin_user_profile.kt

--- kotlin_user_profile.kt
+++ kotlin_user_profile.kt
@@ -1,10 +1,16 @@
+/*This is creating a data class called UserProfile which is used to store user related info*/
 data class UserProfile(
+    /*declaring a property of username. VAL's  are immutable, read-only local variables that can’t be reassigned a different value after its created*/
     val username: String,
+    /*Declaring the property of level to a number*/
     val level: Int,
+    /*declaring the property of isPremium to true or false*/
     val isPremium: Boolean
 )
-
+/*this is the main function where the program starts execution*/
 fun main() {
+    /* this is creating an instance of the UserProfile class. It then adds the values of player 1, level 5, and true*/
     val user = UserProfile("player_01", 5, true)
+    /*this prints all the values out to the screen*/
     println("User: ${user.username}, Level: ${user.level}, Premium: ${user.isPremium}")
 }
