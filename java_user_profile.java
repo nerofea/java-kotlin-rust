4e7557a687b4d8041ebba4d02bf2fd4128d793a5
SweepTheory <joeyripa@gmail.com>
2025-08-01 13:37:24 -0400

    Update java_user_profile.java

--- java_user_profile.java
+++ java_user_profile.java
@@ -20,7 +20,7 @@ class UserProfile {
     }
 /*This is the main method. Strting point of where the program begins to run. Its creating a new UserProfile object and then calls a method to display it*/
     public static void main(String[] args) {
-      /*This is creating a new profile with the value of "player_01", 5, true*/
+      /*This is creating a new profile with the value of "player_01", 5, true and printing it to the screen*/
         UserProfile user = new UserProfile("player_01", 5, true);
         user.printProfile();
     }
