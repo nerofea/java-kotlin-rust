8dc5c91822c4a3115ab545cc7af14c9285978866
SweepTheory <joeyripa@gmail.com>
2025-08-01 20:05:38 -0400

    Update java_hashing.java

--- java_hashing.java
+++ java_hashing.java
@@ -1,7 +1,10 @@
+//import is importing the MessageDigest class from the java.security package to use cryptographic hashing in the code//
 import java.security.MessageDigest;
+//this is importive the NoSuchAlgorithmException from the java.security package. This exception is thrown when a particular cryptographic algorithm is requested but is not available in the environment
 import java.security.NoSuchAlgorithmException;
-
+//creating a public class named auth//
 public class Auth {
+    
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] hash = md.digest(password.getBytes());
