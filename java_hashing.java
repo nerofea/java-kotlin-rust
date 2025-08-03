e5d29f8059e61214f4d6037f1c1b7e840583d5d3
SweepTheory <joeyripa@gmail.com>
2025-08-03 09:44:47 -0400

    Update java_hashing.java

--- java_hashing.java
+++ java_hashing.java
@@ -4,14 +4,23 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 //creating a public class named auth//
 public class Auth {
-    
+    //This is calling a public method for hashPassword which is expecting a string input for password
+    //converts to bytes, then converts to hexidecimal string
+    //and returns a hashed password as a string. 
+    //Throws is in place incase the cryptographic algorithm isnt available
     public static String hashPassword(String password) throws NoSuchAlgorithmException {
+        //this is setting the algorithm to be used as SHA-256
         MessageDigest md = MessageDigest.getInstance("SHA-256");
+         //converts password to bytes and hashes them using SHA-256
         byte[] hash = md.digest(password.getBytes());
+        //this is creating a new StringBuilder Object
         StringBuilder hexString = new StringBuilder();
+        //Looping through each byte of the hashed password
         for (byte b : hash) {
+            //this is converting each byte to hex format
             hexString.append(String.format("%02x", b));
         }
+        //this is returning the new hexidecimal string
         return hexString.toString();
     }
 
