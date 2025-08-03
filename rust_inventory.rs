2cb9133efdc690d72da5958546d17a33141415e9
SweepTheory <joeyripa@gmail.com>
2025-08-03 10:41:44 -0400

    Update rust_inventory.rs

--- rust_inventory.rs
+++ rust_inventory.rs
@@ -1,7 +1,11 @@
+//starts the exectution of code
 fn main() {
+    //assigns the variable inventory the vector of items
     let inventory = vec!["Sword", "Shield", "Potion"];
-
+//prints out the header of Inventory items:
     println!("Inventory items:");
+  // Loops through each item in the vector and prints each item with a dash prefix
+    // Output: -Sword, -Shield, -Potion
     for item in &inventory {
         println!("- {}", item);
     }
