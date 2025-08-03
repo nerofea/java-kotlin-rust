bfdc29c4df913e132114a4df7d607a8d8df296fb
SweepTheory <joeyripa@gmail.com>
2025-08-03 10:23:16 -0400

    Update kotlin_inventory.kt

--- kotlin_inventory.kt
+++ kotlin_inventory.kt
@@ -1,7 +1,12 @@
+//this is calling the main function where the code starts to execute
 fun main() {
+    //creating a variable named inventory with a list of values
     val inventory = listOf("Sword", "Shield", "Potion")
+   //this prints a header for the inventory list
     println("Inventory items:")
+  //this loops through each item in the inventory 
     for (item in inventory) {
+        //prints each item with a dash prefix (- Sword, - Shield, - Potion)
         println("- $item")
     }
 }
